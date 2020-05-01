45c489ff4d4e1d39b03f702141a8d07e7d02610c
Programster <sdpagent@gmail.com>
2020-05-01 13:37:54 +0100

    adds more regions to allowed list of strings

--- enums/AwsRegion.php
+++ enums/AwsRegion.php
@@ -145,15 +145,26 @@ class AwsRegion
     {
         $regionString = strtolower($regionString);
 
+        // https://docs.aws.amazon.com/general/latest/gr/rande.html
         $allowedRegions = array(
             'sa-east-1',
             'us-gov-west-1',
             'ap-northeast-1',
-            'ap-southeast-2',
+            'ap-northeast-2',
+            'ap-northeast-3',
+            'ap-south-1',
             'ap-southeast-1',
+            'ap-southeast-2',
+            'ca-central-1',
+            'eu-central-1',
             'eu-west-1',
+            'eu-west-2',
+            'eu-west-3',
+            'eu-north-1',
+            'eu-south-1',
             'us-west-2',
-            'us-east-1'
+            'us-east-1',
+            'me-south-1'
         );
 
         if (!in_array($regionString, $allowedRegions)) {
