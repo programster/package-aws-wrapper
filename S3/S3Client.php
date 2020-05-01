f3239e10bc78f3b0d6bc3a01be159022f07677d8
Programster <sdpagent@gmail.com>
2020-05-01 17:31:02 +0100

    changes S3Client uploadFile to allow setting null for the ACL in order to not set the ACL. This allows you to still upload files even when you dont have permission to set permissions

--- S3/S3Client.php
+++ S3/S3Client.php
@@ -13,24 +13,24 @@ use function Safe\file_put_contents;
 class S3Client
 {
     private $m_client;
-    
+
     public function __construct($apiKey, $apiSecret, \Programster\AwsWrapper\Enums\AwsRegion $region)
     {
         $credentials = array(
             'key'    => $apiKey,
             'secret' => $apiSecret
         );
-        
+
         $params = array(
             'credentials' => $credentials,
             'version'     => '2006-03-01',
             'region'      => (string) $region,
         );
-        
+
         $this->m_client = new \Aws\S3\S3Client($params);
     }
-    
-    
+
+
     /**
      * Upload a file to s3
      * https://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpPHP.html
@@ -39,7 +39,8 @@ class S3Client
      * @param string $localFilepath - the absolutepath to where the file we want to upload currently is
      * @param string $remoteFilepath - the path you want the file to have inside the bucket, e.g.
      *                                data/data1.csv where "data" is NOT the name of the bucket.
-     * @param Acl $acl - the ACL to store the file with. Refer to the Acl Object.
+     * @param Acl $acl - the ACL to store the file with. If set to null, then an ACL will not be sent in the request.
+     *                   Refer to the ACL Object. You must have WRITE_ACP to be able to set the ACL of an object.
      * @param StorageClass $storageClass - an instance of the StorageClass.
      * @param string $mimeType - the mime type of the file. E.g. text/csv or text/plain
      * @param array $metadata - any name/value pairs you want assosciated with the file.
@@ -49,7 +50,7 @@ class S3Client
         $bucket,
         $localFilepath,
         $remoteFilepath,
-        Acl $acl,
+        ?Acl $acl,
         StorageClass $storageClass,
         $mimeType,
         $metadata = array()
@@ -60,22 +61,26 @@ class S3Client
         if (\iRAP\CoreLibs\StringLib::startsWith($remoteFilepath, '/')) {
             $remoteFilepath = substr($remoteFilepath, 1);
         }
-        
+
         $params = array(
             'Bucket'       => $bucket,
             'Key'          => $remoteFilepath,
             'SourceFile'   => $localFilepath,
             'ContentType'  => $mimeType,
-            'ACL'          => (string) $acl,
             'StorageClass' => (string) $storageClass,
             'Metadata'     => $metadata
         );
-        
+
+        if ($acl !== null)
+        {
+            $params['ACL'] = (string) $acl;
+        }
+
         $result = $this->m_client->putObject($params);
         return $result;
     }
-    
-    
+
+
     /**
      * Upload a file to s3
      * Create a file in s3 with the provided string body.
@@ -106,18 +111,18 @@ class S3Client
         if (\iRAP\CoreLibs\StringLib::startsWith($remoteFilepath, '/')) {
             $remoteFilepath = substr($remoteFilepath, 1);
         }
-        
+
         $params = array(
             'StorageClass' => (string) $storageClass,
             'Metadata'     => $metadata,
         );
-        
+
         if ($mimeType !== '') {
             $params['ContentType'] = $mimeType;
         }
-        
+
         $options = array('params' => $params);
-        
+
         $result = $this->m_client->upload(
             $bucket,
             $remoteFilepath,
@@ -125,11 +130,11 @@ class S3Client
             (string) $acl,
             $options
         );
-        
+
         return $result;
     }
-    
-    
+
+
     /**
      * Upload an entire directory to S3
      * @param string $localDirectoryPath - the path to the directory we wish to upload.
@@ -145,22 +150,22 @@ class S3Client
         if (\iRAP\CoreLibs\StringLib::startsWith($remotePath, "/")) {
             $remotePath = substr($remotePath, 1);
         }
-        
+
         $dest = 's3://' . $bucketName . "/" . $remotePath;
         $aclString = (string)$acl;
-        
+
         $beforeFunc = function (\Aws\CommandInterface $command) use ($aclString) {
             if (in_array($command->getName(), ['PutObject', 'CreateMultipartUpload'])) {
                 $command['ACL'] = $aclString;
             }
         };
-        
+
         $options = array('before' => $beforeFunc);
         $manager = new \Aws\S3\Transfer($this->m_client, $localDirectoryPath, $dest, $options);
         $manager->transfer();
     }
-    
-    
+
+
     /**
      * Download a file from s3. This is only really useful for private files and will generate
      * a pre signed request from the url. If your files are not private, you can just use the
@@ -181,20 +186,20 @@ class S3Client
             'Bucket' => $bucket,
             'Key'    => $key
         ]);
-        
+
         $expires = '+1 minutes';
         $request = $this->m_client->createPresignedRequest($cmd, $expires);
         $presignedUrl = (string) $request->getUri();
         $fileHandle = @fopen($presignedUrl, 'r');
-        
+
         if ($fileHandle === false) {
             throw new \Exception("Failed to find/open the S3 file.");
         }
-        
+
         file_put_contents($downloadFilepath, $fileHandle);
     }
-    
-    
+
+
     /**
      * Get a pre-signed download link from s3. This is only really useful for private files and will generate
      * a pre signed request from the url. If your files are not private, you can just use the
@@ -214,14 +219,14 @@ class S3Client
             'Bucket' => $bucket,
             'Key'    => $key
         ]);
-        
+
         $expires = '+' . $ttl . ' seconds';
         $request = $this->m_client->createPresignedRequest($cmd, $expires);
         $presignedUrl = (string) $request->getUri();
         return $presignedUrl;
     }
-    
-    
+
+
     /**
      * Delete a bucket and all of its contents from S3
      * @param string $bucketName - the name of the bucket you wish to delete.
@@ -232,13 +237,13 @@ class S3Client
         $params = array(
             'Bucket' => $bucketName
         );
-        
+
         $result = $this->m_client->deleteBucket($params);
-        
+
         return $result;
     }
-    
-    
+
+
     /**
      * List all the buckets in your region.
      */
@@ -247,8 +252,8 @@ class S3Client
         $result = $this->m_client->listBuckets(array());
         return $result;
     }
-    
-    
+
+
     /**
      * Returns some or all (up to 1000] of the objects in a bucket. You can use the request
      * parameters as selection criteria to return a subset of the objects in a bucket.
@@ -264,32 +269,32 @@ class S3Client
         string $prefix = "",
         int $maxKeys = 1000,
         string $delimiter = ""
-    ) : \Programster\AwsWrapper\Responses\ResponseListObjects 
+    ) : \Programster\AwsWrapper\Responses\ResponseListObjects
     {
         // If the user provided a starting slash, this is a mistake and needs to be stripped.
         // for some reason paths are not like linux absolute paths
         if (\iRAP\CoreLibs\StringLib::startsWith($prefix, "/")) {
             $prefix = substr($prefix, 1);
         }
-        
+
         $params = array(
             'Bucket'        => $bucket, // REQUIRED
             'MaxKeys'       => $maxKeys,
         );
-        
+
         if ($prefix !== "") {
             $params['Prefix'] = $prefix;
         }
-        
+
         if ($delimiter !== "") {
             $params['Delimiter'] = $delimiter;
         }
-        
+
         $awsResponse = $this->m_client->listObjects($params);
         return new \Programster\AwsWrapper\Responses\ResponseListObjects($awsResponse);
     }
-    
-    
+
+
     /**
      * Create a bucket
      * https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-s3-2006-03-01.html#createbucket
@@ -301,12 +306,12 @@ class S3Client
             'ACL'    => (string) $acl,
             'Bucket' => $bucketName
         );
-        
+
         $result = $this->m_client->createBucket($params);
         return $result;
     }
-    
-    
+
+
     /**
      * Download an entire bucket to a local directory.
      * @param string $bucketName - the name of the bucket we are grabbing.
@@ -317,10 +322,10 @@ class S3Client
         if (!is_dir($destination)) {
             throw new Exception('Destination needs to be a path to a folder,');
         }
-        
+
         // Where the files will be source from
         $source = 's3://' . $bucketName;
-        
+
         $manager = new \Aws\S3\Transfer($this->m_client, $source, $destination);
         $manager->transfer();
     }
@@ -337,11 +342,11 @@ class S3Client
     public function deleteObjects(string $bucket, array $names, array $extraParams = array())
     {
         $objectsArray = [];
-        
+
         foreach ($names as $name) {
             $objectsArray[] = array('Key' => $name);
         }
-        
+
         $params = array(
             'Bucket' => $bucket,
             'Delete' => [
@@ -349,17 +354,17 @@ class S3Client
                 'Quiet' => true,
             ]
         );
-        
+
         if (count($extraParams) > 0)
         {
             $params = array_merge($params, $extraParams);
         }
-        
+
         $response = $this->m_client->deleteObjects($params);
         return $response;
     }
-    
-    
+
+
     /**
      * Delete a "folder" within an S3 bucket. E.g. you want to delete all objects underneath /folder1
      * @param string $bucketName - the name of the bucket to delete from
@@ -370,21 +375,21 @@ class S3Client
         if (\iRAP\CoreLibs\StringLib::startsWith($path, '/')) {
             $path = substr($path, 1);
         }
-        
+
         $isTruncated = true;
-        
+
         while ($isTruncated) {
             $listObjectsResponse = $this->listObjects($bucketName, $path);
             $isTruncated = $listObjectsResponse->isTruncated();
             $objects = $listObjectsResponse->getObjects();
             $collection = [];
-            
+
             if (count($objects) > 0) {
                 foreach ($objects as $object) {
                     /* @var $object \Programster\AwsWrapper\Objects\S3Object */
                     $collection[] = $object->getKey();
                 }
-                
+
                 $this->deleteObjects($bucketName, $collection);
             }
         }
@@ -392,7 +397,7 @@ class S3Client
 
 
     /**
-     * Loops through the objects in a bucket, executing your callback on each one. 
+     * Loops through the objects in a bucket, executing your callback on each one.
      * Use this instead of listObjects if you need to not worry about maxKeys being 1000.
      * @param string $bucket - the name of the bucket that contains your objects.
      * @param string $prefix - Limits the response to keys that begin with the specified prefix.
@@ -402,7 +407,7 @@ class S3Client
     {
         $iterator = $this->m_client->getIterator('ListObjects', array('Bucket' => $bucket, 'Prefix' => $prefix));
 
-        foreach ($iterator as $object) 
+        foreach ($iterator as $object)
         {
             $callback->handle(new \Programster\AwsWrapper\Objects\S3Object($object));
         }
