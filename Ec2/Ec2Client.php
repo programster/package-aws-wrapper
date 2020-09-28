f2b5b295c28f2ce26baad8e98b6e2a3e860f9f0b
Stuart Page <sdpagent@gmail.com>
2020-09-28 16:45:43 +0100

    impelements methods/request-objects for starting/stopping ec2 instances. Fixes issue with requestOnDemandInstances alias.

--- Ec2/Ec2Client.php
+++ Ec2/Ec2Client.php
@@ -43,6 +43,8 @@ class Ec2Client
     /**
      * Launch some on demand instances (fixed price).
      * http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-ec2-2015-04-15.html#runinstances
+     * @param \Programster\AwsWrapper\Requests\RequestRunInstances $request
+     * @return type
      */
     public function runInstances(\Programster\AwsWrapper\Requests\RequestRunInstances $request)
     {
@@ -52,13 +54,34 @@ class Ec2Client
     
     /**
      * Alias for RunInstances
+     * @param \Programster\AwsWrapper\Requests\RequestRunInstances $request
      */
-    public function requestOnDemandInstances()
+    public function requestOnDemandInstances(\Programster\AwsWrapper\Requests\RequestRunInstances $request)
     {
-        $this->runInstances();
+        $this->runInstances($request);
     }
     
     
+    /**
+     * Sent a request to start some stopped instances. This does NOT "create" instances, use "runInstances()" for that.
+     * https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-ec2-2016-11-15.html#startinstances
+     */
+    public function startInstances(\Programster\AwsWrapper\Requests\RequestStartInstances $request)
+    {
+        return $request->send($this->m_client);
+    }
+
+
+    /**
+     * Sent a request to start some stopped instances. This does NOT "terminate" instances.
+     * https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-ec2-2016-11-15.html#startinstances
+     */
+    public function stopInstances(\Programster\AwsWrapper\Requests\RequestStopInstances $request)
+    {
+        return $request->send($this->m_client);
+    }
+
+
     /**
      * Send a request to terminate instances.
      * http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-ec2-2015-04-15.html#terminateinstances
---
+++ requests/RequestStopInstances.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Programster\AwsWrapper\Requests;
+
+/*
+ * A request to terminate a single or multiple instances.
+ */
+
+class RequestStopInstances extends Ec2RequestAbstract
+{
+    private $m_instanceIds = array();
+
+
+    /**
+     * Create a request to terminate one or more ec2 instances.
+     * @param AmazonRegion $region - the region the instances are located in.
+     * @param mixed $instance_ids - a single instance id or an array of instance ids
+     */
+    public function __construct(string ...$instance_ids)
+    {
+        if (count($instance_ids) === 0)
+        {
+            $errMsg = get_called_class() . ': need to provide at least one string ID of an instance to stop.';
+            throw new \Exception($errMsg);
+        }
+
+        $this->m_instanceIds = $instance_ids;
+    }
+
+
+    protected function getOptionsArray()
+    {
+        return array(
+            'InstanceIds' => $this->m_instanceIds
+        );
+    }
+
+
+    protected function sendRequest(\Aws\Ec2\Ec2Client $ec2, array $opt)
+    {
+        $response = $ec2->stopInstances($opt);
+        return $response;
+    }
+}
